9818850022dae49043c7fd24c3f66552aad5dd54
BenMiriello <benmiriello@gmail.com>
2021-04-18 11:44:18 -0400

    Add scrolling for suggested goals

--- src/screens/Suggestions/Suggestions.tsx
+++ src/screens/Suggestions/Suggestions.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import SuggestedGoalCards from './SuggestedGoalCards';
 
 export default function Suggestions(){
   return(
-    <View style={styles.container}>
+    <ScrollView>
       <View style={styles.body}>
         <SuggestedGoalCards />
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    width: '100%'
-  },
   body: {
-    height: '90%',
     display: "flex",
     justifyContent: 'space-around',
     flexDirection: "row",
