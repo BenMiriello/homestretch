893ad06506070249b9247945de92337174f0680e
BenMiriello <benmiriello@gmail.com>
2021-04-21 19:54:30 -0400

    Add dummy saveGoalToDb function in state

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -1,18 +1,35 @@
 import React, { createContext, useContext, useState } from 'react';
 
+export interface GoalType {
+  name: string;
+  timesPerWeek: number;
+  weekdays: string;
+  shared: boolean;
+};
+
 export interface StateContextType {
   goalName: string;
   setGoalName: React.Dispatch<string>;
+  saveGoalToDb: (g: GoalType) => GoalType;
 };
 
 export const StateContext = createContext<StateContextType>(null!);
 
 export const AppStateProvider = (props: React.PropsWithChildren<{}>) => {
   const [goalName, setGoalName] = useState('');
+  const saveGoalToDb = (newGoal: GoalType) => {
+    // fetch()
+    // .then(r => r.json())
+    // .then(res => )
+    // return res.goal;
+    setGoalName('');
+    return newGoal;
+  };
 
   const contextValue = {
     goalName,
     setGoalName,
+    saveGoalToDb,
   } as StateContextType;
 
   return <StateContext.Provider value={contextValue}>{props.children}</StateContext.Provider>;
