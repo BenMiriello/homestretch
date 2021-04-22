d9efe1acdc0e7982da8592230f3e4b185c42674b
BenMiriello <benmiriello@gmail.com>
2021-04-22 08:56:29 -0400

    Add minimal styling to goal details page

--- src/screens/GoalDetails/GoalDetails.tsx
+++ src/screens/GoalDetails/GoalDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
 
 import useAppState from '../../state';
 
@@ -7,8 +7,34 @@ export default function GoalDetails() {
   const { currentGoal } = useAppState();
 
   return (
-    <View>
-      <Text>{currentGoal.name}</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.textRow}>{currentGoal.name}</Text>
+          <Text style={styles.textRow}>{currentGoal.timesPerWeek.toString()}</Text>
+          <Text style={styles.textRow}>{currentGoal.weekdays}</Text>
+          <Text style={styles.textRow}>{currentGoal.shared ? 'Public' : 'Private'}</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    display: "flex",
+    justifyContent: 'space-around',
+    flexDirection: "column",
+    alignItems: 'center',
+    flexWrap: "wrap",
+    paddingTop: 10,
+    paddingLeft: '5%',
+    paddingRight: '5%',
+  },
+  textRow: {
+    height: 50,
+    fontSize: 20,
+  },
+});
