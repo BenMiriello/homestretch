dafb58f7fa470ea1cfef8e1b4510e02de5d1ad8e
BenMiriello <benmiriello@gmail.com>
2021-04-23 21:44:10 -0400

    Rename types file -> constants

--- src/screens/GoalDetails/GoalDetails.tsx
+++ src/screens/GoalDetails/GoalDetails.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
 import useAppState from '../../state';
-import { weekDayNames } from '../../../types';
+import { weekDayNames } from '../../../constants';
 
 export default function GoalDetails() {
   const { currentGoal } = useAppState();
